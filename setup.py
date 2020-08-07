27e5272064dad32a7bec2e7c7e658cf2d9c78fb6
Kasper Munch <kaspermunch@birc.au.dk>
2020-08-07 22:29:48 +0200

    Bumped verison

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='slurm-jupyter-run',
-      version='1.0.4',
+      version='1.0.5',
       author='Kasper Munch',
       description='Utility for notebooks on a slurm cluster.',
       # long_description=long_description,
